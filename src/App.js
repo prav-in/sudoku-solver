84ed676f2e765064cb8d7284f9a65c63452d17b6
Pravin <prav.ch5@gmail.com>
2022-06-08 11:15:42 +0530

    updated header and footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/header';
 import Footer from "./components/footer";
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Footer/>
     </div>
   );
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ import "./css/footer.css"
 const footer = () => {
   return (
     <footer className="footer">
-<p className='about'>Sudoku is a logic-based, combinatorial number-placement puzzle. This Web App is Created using React to Provide Solutions to your Sudoku Puzzles</p>
+<p className='footer-about'>Sudoku is a logic-based, combinatorial number-placement puzzle. This Web App is Created using React to Provide Solutions to your Sudoku Puzzles</p>
 <div className='footer__links'>
     <a href='https://github.com/prav-in'>prav-in</a>  
     <a href='https://github.com/kumawatsr'>kumawatsr</a> 
