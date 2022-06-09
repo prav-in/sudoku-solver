274a14b230c9a72d9e128f14917b1cef0fb6b31e
Pravin <prav.ch5@gmail.com>
2022-06-09 13:55:53 +0530

    built sudoku grid and algorithm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header';
 import Footer from "./components/footer";
+import Main from './components/Main';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
       <Footer/>
     </div>
   );
